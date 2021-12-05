535e0f0f6eeb1f63c2b85a10710a616dedc2d829
shenshi <shenshi@shouqianba.com>
2021-12-05 15:55:13 +0800

    webpack 使用 mfsu

---
+++ src/App/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Antd from '../pages/Antd'
+import FramerMotion from '../pages/FramerMotion'
+
+const App: React.FC = ({}) => {
+  return (
+    <div>
+      <Antd />
+      <FramerMotion />
+    </div>
+  )
+}
+
+export default App
